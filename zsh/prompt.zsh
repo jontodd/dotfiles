84493067ccdc234911ad7659e477e583aa84ca71
Jon Todd <jtodd@okta.com>
2019-04-05 16:58:07 -0700

    Working prompt

--- zsh/prompt.zsh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-
-POWERLEVEL9K_MODE='nerdfont-complete'
-source ~/jontodd/dotfiles/libs/github/powerlevel9k/powerlevel9k.zsh-theme
---
+++ zsh/zsh.d/prompt.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+POWERLEVEL9K_MODE='nerdfont-complete'
+source ~/.zsh_themes/powerlevel9k/powerlevel9k.zsh-theme
